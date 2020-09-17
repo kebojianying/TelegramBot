83001e1c257ce5ce0bedab0b92fed549a7c6f5a4
kebojianying <tolamitouattcks@gmail.com>
2020-09-17 12:35:15 +0800

    jkjk

---
+++ src/main/java/com/syc/sycsf/NioTest.java
@@ -0,0 +1,12 @@
+package com.syc.sycsf;
+
+import java.nio.IntBuffer;
+
+public class NioTest {
+    public static void main(String[] args){
+        //创建一个buffer ，里面有
+        IntBuffer intBuffer = IntBuffer.allocate(5);
+
+
+    }
+}
